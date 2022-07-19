581b990a6f1d5f14fcfff106871bae56e11cc182
Daniel <daniel_chrzanowski98@wp.pl>
2022-07-19 14:15:10 +0200

    material import optimize

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-//import { MatCardModule } from '@angular/material/card';
-//import { MatCheckboxModule } from '@angular/material/checkbox';
-//import { MatAutocompleteModule } from '@angular/material/autocomplete';
-//import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
